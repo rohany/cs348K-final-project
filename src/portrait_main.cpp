2d207fc5fae5fca51ee19a4c38162046f2fd5855
Rohan Yadav <rohany@alumni.cmu.edu>
2021-05-09 11:26:18 -0700

    src: add a command line argument parser

--- src/portrait_main.cpp
+++ src/portrait_main.cpp
@@ -8,17 +8,43 @@
 #include "HalideRuntime.h"
 #include "halide_image_io.h"
 
-int main() {
-  // TODO (rohany): Do some command line argument parsing.
-  Halide::Runtime::Buffer<uint8_t> inLeft = Halide::Tools::load_image("dude-left.jpg");
-  Halide::Runtime::Buffer<uint8_t> inRight = Halide::Tools::load_image("dude-right.jpg");
+#include "args.hxx"
+
+int main(int argc, char** argv) {
+
+  args::ArgumentParser parser("Runs the portrait mode generator.", "");
+  args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+
+  args::ValueFlag<std::string> left(parser, "left image", "left stereo image", {'l', "left"});
+  args::ValueFlag<std::string> right(parser, "right image", "right stereo image", {'r', "right"});
+  args::ValueFlag<std::string> output(parser, "output image", "output image filename", {'o', "output"});
+
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (args::Help) {
+    std::cout << parser;
+    return 0;
+  } catch (args::ParseError e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
+  }
+  catch (args::ValidationError e)
+  {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
+  }
+
+  Halide::Runtime::Buffer<uint8_t> inLeft = Halide::Tools::load_image(args::get(left));
+  Halide::Runtime::Buffer<uint8_t> inRight = Halide::Tools::load_image(args::get(right));
   Halide::Runtime::Buffer<uint8_t> out(inLeft.width(), inLeft.height(), inLeft.channels());
 
 
   auto result = portrait_gen(inLeft, inRight, out);
   assert(result == 0);
 
-  Halide::Tools::save_image(out, "dude-depth.jpg");
+  Halide::Tools::save_image(out, args::get(output));
 
   return 0;
 }
